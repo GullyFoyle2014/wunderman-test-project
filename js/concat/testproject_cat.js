db7f874d156517711b57c4b201e89bf9536c20aa
GullyFoyle2014 <alex.m2004@gmail.com>
2016-01-06 22:21:47 -0500

    adding project to github repo to test Hound watch

--- js/concat/testproject_cat.js
+++ js/concat/testproject_cat.js
@@ -1,27 +1,38 @@
 //test project
 $(document).ready(function(){
 
-
 	function resizeDiv() {
 		vpw = $(window).width();
 		vph = $(window).height();
 		$('#head').css({'height': vph + 'px'});
 	}
-	resizeDiv();
+	
+	function ifNoTouch(){
+		if( $('html.no-touch') ){
+			$('.imagesGridLink').on('mouseenter', function(e){
+			    e.preventDefault();
+			    $('.imagesGridCaption').hide();
+			    $('.imagesGridLink').removeClass('opaque show');
+			    $(this).toggleClass('opaque show');
+			    $(this).next('div').show();
+			});
+		}//end if viewportwidth
+	}//end ifNoTouch function
+	
+	function modalImage(){
+		$(".media").on("click", function() {
+		   $('#imagepreview').attr('src', $(this).find('.imageresource').attr('src'));
+		   $('#imagemodal').modal('show');
+		});
+	}
 
-	$('.imagesGridLink').on('mouseenter', function(e){
-	    e.preventDefault();
-	    $('.imagesGridCaption').hide();
-	    $('.imagesGridLink').removeClass('opaque show');
-	    $(this).toggleClass('opaque show');
-	    $(this).next('span').show();
-	});
+	//fire these off at doc.ready
+	resizeDiv();
+	ifNoTouch();
+	modalImage();
 
 	window.onresize = function(event) {
 		resizeDiv();
-
 	};
 
 });//end doc.ready
-
-
